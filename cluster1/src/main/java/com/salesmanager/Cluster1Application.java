34d46742d65bdb627fe2f524ced76ed829eb76ed
Cloud User <centos@ip-10-0-1-106.ec2.internal>
2020-05-27 13:45:33 +0000

    push from ec2

---
+++ cluster1/src/main/java/com/salesmanager/Cluster1Application.java
@@ -0,0 +1,13 @@
+package com.salesmanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Cluster1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Cluster1Application.class, args);
+	}
+
+}
