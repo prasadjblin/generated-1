450029b6034388e826463575f041c47f0513c220
Cloud User <centos@ip-10-0-1-106.ec2.internal>
2020-05-28 07:39:55 +0000

    push from ec2

---
+++ cluster1/src/test/java/com/salesmanager/Cluster1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.salesmanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Cluster1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
